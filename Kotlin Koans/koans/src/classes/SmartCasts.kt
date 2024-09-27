3d0407f22a8d10391e703308f0bbb51d772497ba
Daniel Brito <daniell-henrique@hotmail.com>
2024-09-27 16:17:25 -0300

    Add files via upload

---
+++ Kotlin Koans/koans/src/classes/SmartCasts.kt
@@ -0,0 +1,24 @@
+package classes
+
+fun eval(expr: Expr): Int =
+    when (expr) {
+        is Num -> expr.value
+        is Sum -> eval(expr.left) + eval(expr.right)
+        else -> throw IllegalArgumentException("Unknown expression")
+    }
+
+interface Expr
+
+class Num(
+    val value: Int,
+) : Expr
+
+class Sum(
+    val left: Expr,
+    val right: Expr,
+) : Expr
+
+fun main() {
+    println(eval(Num(1)))
+    println(eval(Sum(Num(1), Num(2))))
+}

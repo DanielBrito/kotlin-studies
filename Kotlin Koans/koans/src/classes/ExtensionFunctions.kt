56fbad438848ba7784c9c81d6dd0911a5d2c6282
Daniel Brito <daniell-henrique@hotmail.com>
2024-10-11 16:07:26 -0300

    Create ExtensionFunctions.kt

---
+++ Kotlin Koans/koans/src/classes/ExtensionFunctions.kt
@@ -0,0 +1,15 @@
+package classes
+
+fun Int.r(): RationalNumber = RationalNumber(this, 1)
+
+fun Pair<Int, Int>.r(): RationalNumber = RationalNumber(first, second)
+
+data class RationalNumber(
+    val numerator: Int,
+    val denominator: Int,
+)
+
+fun main() {
+    println(2.r())
+    println(Pair(2, 3).r())
+}

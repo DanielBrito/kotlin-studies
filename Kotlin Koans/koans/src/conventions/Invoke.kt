293389b7f47678bf3a0f6c5b6f9f870d28dc652a
Daniel Brito <daniell-henrique@hotmail.com>
2024-10-16 13:03:20 -0300

    Create Invoke.kt

---
+++ Kotlin Koans/koans/src/conventions/Invoke.kt
@@ -0,0 +1,21 @@
+package conventions
+
+class Invokable {
+    var numberOfInvocations: Int = 0
+        private set
+
+    operator fun invoke(): Invokable {
+        numberOfInvocations++
+        return this
+    }
+}
+
+fun invokeTwice(invokable: Invokable) = invokable()()
+
+fun main() {
+    val invokable = Invokable()
+
+    invokeTwice(invokable)
+
+    println(invokable.numberOfInvocations) // 2
+}
